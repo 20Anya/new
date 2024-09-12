36670da3f74b473f49fcd0e928704b232c0d2813
Anya Arkhipova <aana45751@gmail.com>
2024-09-12 15:55:48 +0300

    task 3

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
@@ -19,6 +18,24 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_value(self):
+        if not self.grades:
+            return 0
+        all_sum = 0
+        all_count = 0
+        for value_grades_for_one_course in self.grades.values():
+            all_sum = all_sum + sum(value_grades_for_one_course)
+            all_count = all_count + len(value_grades_for_one_course)
+        return all_sum / all_count
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за домашнее задание: {self.average_value()}\n'
+                f'Курсы в процессе изучения: {self.courses_in_progress}\n'
+                f'Завершенные курсы: {self.finished_courses}')
+    def __lt__(self, other):
+        return self.average_value() < other.averge_value()
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,12 +47,36 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
-        self.lecture_grades = []
+        self.lecture_grades = {}
+
+    def average(self):
+        if not self.lecture_grades:
+            return 0
+        all_sum = 0
+        all_count = 0
+        for value_grades_for_one_course in self.lecture_grades.values():
+            all_sum = all_sum + sum(value_grades_for_one_course)
+            all_count = all_count + len(value_grades_for_one_course)
+        return all_sum / all_count
+
+    def __lt__(self, other):
+        return self.average() < other.averge()
+    
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.average()}')
+
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}')
 
     def rate_rw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -45,3 +86,15 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+some_reviewer = Reviewer('Some', 'Buddy')
+some_lecturer = Lecturer('Some', 'Buddy')
+some_student = Student('Ruoy', 'Eman')
+
+some_lecturer.courses_attached += ['Python']
+
+some_student.rate_st(some_lecturer, 'Python', 10)
+some_student.rate_st(some_lecturer, 'Python', 10)
+some_student.rate_st(some_lecturer, 'Python', 10)
+
+print(some_lecturer)
